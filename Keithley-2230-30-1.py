6cc432fe10bb2413ae8ccf6e5b803d67470d971c
Tobias Guggemos <tobias.guggemos@univie.ac.at>
2025-07-25 10:42:02 +0200

    added mutex for reading serial and added functionality for increasing/decreasing max current

--- Keithley-2230-30-1.py
+++ Keithley-2230-30-1.py
@@ -6,27 +6,31 @@ import os
 # Define initial values
 #USB_PORT='ASRL/dev/ttyUSB3::INSTR'
 USB_PORT='USB0::1510::8752::9031683::0::INSTR'
-names = ['Channel 1','Channel 2', 'Channel 2']
+names=['Channel 1', 'Channel 2', 'Channel 3']
 toggle_allowed=[True,True,True]
 voltage = [0, 0, 0]
 current = ['0', '0', '0']
+max_current=['0','0','0']
 status = [False, False, False]
 rm = None
 inst = None
 device = ''
 error_msg = ''
 error_counter = 0
+connected=False
+mutex=threading.Lock()
 
 def connect_usb():
-    global device, inst, rm
+    global device, inst, rm, connected
     print("try to connect ot usb")
-    while True:
+    while not connected:
         try:
-            device = "Try to connect to USB"
-            rm = pyvisa.ResourceManager()
-            inst = rm.open_resource(USB_PORT)
-            device = inst.query('*IDN?')
-            break;
+            with mutex:
+                device = "Try to connect to USB"
+                rm = pyvisa.ResourceManager()
+                inst = rm.open_resource(USB_PORT)
+                device = inst.query('*IDN?')
+                connected=True
         except:
             print("Error connicting to USB, restart usb port")
             os.system("usbreset 05e6:2230")
@@ -35,24 +39,26 @@ def connect_usb():
 
 # Function to simulate reading voltage and current values
 def read_voltage_current():
-    global voltage, current, status, device
+    global voltage, current, max_current, status, device, connected
 
-    while inst == None:
+    while inst == None or not connected:
         connect_usb()
         print("USB connected")
     while True:
         try:
 #            inst = rm.open_resource(USB_PORT)
-            device = inst.query('*IDN?')
-            for i in range(len(names)):
-                inst.write(f'INSTRUMENT:SELECT CH{i+1}')
-                voltage[i] = inst.query(f'MEASURE:VOLTAGE?').rstrip() # Example: Replace with actual reading logic
-                current[i] = inst.query(f'MEASURE:CURRENT?').rstrip()   # Example: Replace with actual reading logic
-                status[i] = inst.query(f'CHANNEL:OUTPUT?').rstrip() # Replace
-                if status[i] == '1' or status[i] == 'ON':
-                    status[i] = True
-                elif status[i] == '0' or status[i] == 'OFF':
-                    status[i] = False
+            with mutex:
+                device = inst.query('*IDN?')
+                for i in range(len(names)):
+                    inst.write(f'INSTRUMENT:SELECT CH{i+1}')
+                    voltage[i] = inst.query(f'MEASURE:VOLTAGE?').rstrip() # Example: Replace with actual reading logic
+                    current[i] = f"{inst.query(f'MEASURE:CURRENT?').rstrip()}"
+                    max_current[i]=f"{inst.query(f'SOURCE:CURRENT?').rstrip()} (+/- {inst.query(f'SOURCE:CURRENT:STEP?').rstrip()})"   # Example: Replace with actual reading logic
+                    status[i] = inst.query(f'CHAN:OUTP?').rstrip() # Replace
+                    if status[i] == '1' or status[i] == 'ON':
+                        status[i] = True
+                    elif status[i] == '0' or status[i] == 'OFF':
+                        status[i] = False
         except Exception as e:
             print(f"caught annoying error {e}")
 #        finally:
@@ -62,15 +68,43 @@ def read_voltage_current():
 #                print('No session to close')
         time.sleep(1)
 
+def change_current_rate(up_down):
+    with mutex:
+        for i in range(len(names)):
+            inst.write(f'INSTRUMENT:SELECT CH{i+1}')
+            old_val = float(inst.query(f'SOURCE:CURRENT:STEP?').rstrip())
+            new_val = min(max(round(old_val + up_down*0.01,2),0.001),1.0)
+            inst.write(f'SOURCE:CURRENT:STEP {new_val}A')
+ 
+def change_current(index, up_down):
+    global status, error_msg
+    if toggle_allowed[index]:
+        try:
+            with mutex:
+                on_off = int(not status[index])
+                inst.write(f'INSTRUMENT:SELECT CH{index+1}')
+                inst.write(f'SOURCE:CURRENT:STEP 0.1A')
+                if up_down == 1:
+                    inst.write(f'SOURCE:CURRENT:UP')
+                elif up_down == -1:
+                    inst.write(f'SOURCE:CURRENT:DOWN')
+        except:
+            status[index] = not status[index]
+    else:
+        error_msg="Toggle not allowed for this Channel"
+        status[index] = not status[index]
+
+
 # Function to handle the on/off toggle action
 def toggle_status(index):
     global status, error_msg
     if toggle_allowed[index]:
         try:
 #            inst = rm.open_resource(USB_PORT)
-            on_off = int(not status[index])
-            inst.write(f'INSTRUMENT:SELECT CH{index+1}')
-            inst.write(f'CHANNEL:OUTPUT {on_off}')
+            with mutex:
+                on_off = int(not status[index])
+                inst.write(f'INSTRUMENT:SELECT CH{index+1}')
+                inst.write(f'CHANNEL:OUTPUT {on_off}')
 #            ret = inst.query(f":OUTPut{index+1}:STATe {on_off}", 3)
         except:
             status[index] = not status[index]
@@ -100,12 +134,25 @@ def init_screen(stdscr):
     curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_BLACK)  # Transparent color for OFF state
     curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK)  # RED for important stuff
 
-    grid_height = 10
-    grid_width=30
+    grid_height = 12
+    grid_width=40
     starts = []
     def draw_grid():
         stdscr.clear()
         height, width = stdscr.getmaxyx()
+
+        min_cols, min_lines = 120, 20
+        try:
+            size = os.get_terminal_size()
+            if size.columns < min_cols or size.lines < min_lines:
+                stdscr.clear()
+                stdscr.addstr(0, 0, f"[Error] The terminal winodw is too small. Please increase to {min_cols}x{min_lines}.")
+                stdscr.addstr(2, 0, "Press any key to retry...")
+                stdscr.refresh()
+                stdscr.getch()
+                return
+        except OSError:
+            pass
 
         stdscr.addstr(0, 1, f'Connected to {device}')
         # Define grid40
@@ -139,9 +186,11 @@ def init_screen(stdscr):
                 stdscr.addstr(start_y + 3, start_x + 1, f"Channel: {i * cols + 1 + j}")
                 stdscr.addstr(start_y + 4, start_x + 1, f"Voltage: {voltage[i * cols + j]}")
                 stdscr.addstr(start_y + 5, start_x + 1, f"Current: {current[i * cols + j]}")
-                stdscr.addstr(start_y + 6, start_x + 1, f"Status:  {'ON' if status[i * cols + j] else 'OFF'}")
+                stdscr.addstr(start_y + 6, start_x + 1, f"Max_Cur: {max_current[i * cols + j]}")
+                stdscr.addstr(start_y + 7, start_x + 1, f"Status:  {'ON' if status[i * cols + j] else 'OFF'}")
                 stdscr.attroff(curses.color_pair(color_pair))
-                stdscr.addstr(start_y + 8, start_x + 1, f"Press {chr(ord('1')+i*cols+j)} to toggle ON/OFF".center(grid_width-2))
+                stdscr.addstr(start_y + 9, start_x + 1, f"Press {chr(ord('1')+i*cols+j)} to toggle ON/OFF".center(grid_width-2))
+                stdscr.addstr(start_y + 10, start_x + 1, f"Max_cur: -:F{chr(ord('1')+2*(i*cols+j))}, +:F{chr(ord('2')+2*(i*cols+j))}".center(grid_width-2))
 
 #        start_y = grid_height*2+2
 #        stdscr.addstr(start_y, 1, f"Available Resources:")
@@ -157,7 +206,8 @@ def init_screen(stdscr):
 #            else:
 #                stdscr.addstr(start_y+i, 1, f"({key}): {dev}")
        
-        stdscr.addstr(start_y+8+i+2, 1, f"Press q to quit")
+        stdscr.addstr(start_y+10+i+2, 1, f"Press +/- to increase/decrease the CURRENT stepsize by +/- 0.01")
+        stdscr.addstr(start_y+11+i+2, 1, f"Press q to quit")
         stdscr.refresh()
 
     def error_message(text):
@@ -233,6 +283,22 @@ def init_screen(stdscr):
                 confirm_toggle(2)
             elif key == ord('4'):
                 confirm_toggle(3)
+            elif key == ord('+'):
+                change_current_rate(up_down=+1)
+            elif key == ord('-'):
+                change_current_rate(up_down=-1)
+            elif key == curses.KEY_F1:
+                change_current(0,-1)
+            elif key == curses.KEY_F2:
+                change_current(0,+1)
+            elif key == curses.KEY_F3:
+                change_current(1,-1)
+            elif key == curses.KEY_F4:
+                change_current(1,+1)
+            elif key == curses.KEY_F5:
+                change_current(2,-1)
+            elif key == curses.KEY_F6:
+                change_current(2,+1)
             draw_grid()
 
     print("Start main loop")
